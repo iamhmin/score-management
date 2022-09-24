2a411c2e09f01e0eebc5b79260af268dc2706d52
hamin seong <iamhmin09@gmail.com>
2022-09-24 15:03:12 +0900

    first commit

---
+++ src/main/java/com/scoremanagement/ScoreManagementApplication.java
@@ -0,0 +1,13 @@
+package com.scoremanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoreManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoreManagementApplication.class, args);
+	}
+
+}
